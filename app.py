67759f2051aa0007fc57332e7d9eb24623d8f343
Nem T. Lea <nemtielea@gmail.com>
2024-04-16 22:33:01 +0200

    dalsze dodawanie nowych funkcji do programu

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 print("""Czesc! Jestem programem ktory wysyla paczki.
-Podaj element oraz wage elementu w kg aby okreslic ilosc calkowita do wyslania
-wpisz 'wyslij' aby wyslac utworzone paczki.
-Nacisnij enter z pustym polem aby zakonczyc proces tworzenia paczek""")
+Podaj element oraz wage elementu w kg aby okreslic ilosc calkowita do wyslania.
+Wpisz liczbe mniejsza od 1 lub wieksza od 20 aby wyslac utworzone paczki.""")
 
 print("-" * 40)
 
@@ -10,35 +9,41 @@ ilosc_paczek = 0
 puste_kilogramy = 0
 ilosc_elementow = 0
 najwieksza_ilosc_pustych_elementow = 0
+paczka = 0
 
 while True:
     waga = int(input("Podaj wage elementu do wyslania: "))
     if waga < 1:
         puste_kilogramy = ilosc_paczek * 20 - ilosc_wagi
+        najwieksza_ilosc_pustych_elementow = 20 - ilosc_wagi
         print(f"""Zakonczono dodawania paczek. Ilosc wyslanych paczek: {ilosc_paczek}
         puste kilogramy: {puste_kilogramy}
         ilosc elementow: {ilosc_elementow}
-        najwiecej pustych elementow w paczce nr.: {najwieksza_ilosc_pustych_elementow}""")
+        najwiecej pustych elementow w paczce nr.: {najwieksza_ilosc_pustych_elementow}kg""")
         break
     if waga > 20:
         puste_kilogramy = ilosc_paczek * 20 - ilosc_wagi
+        najwieksza_ilosc_pustych_elementow = 20 - ilosc_wagi
         print(f"""Zakonczono dodawania paczek. Ilosc wyslanych paczek: {ilosc_paczek}
     puste kilogramy: {puste_kilogramy}
     ilosc elementow: {ilosc_elementow}
-    najwiecej pustych elementow w paczce nr.: {najwieksza_ilosc_pustych_elementow}""")
+    najwiecej pustych elementow w paczce nr.: {najwieksza_ilosc_pustych_elementow}kg""")
         break
     if waga < 20:
         ilosc_wagi += waga
         ilosc_elementow += 1
+        najwieksza_ilosc_pustych_elementow = 20 - ilosc_wagi
         print(f"Podana waga: {waga}")
     if waga == 20:
         ilosc_paczek += 1
         ilosc_elementow += 1
+        najwieksza_ilosc_pustych_elementow = 20 - ilosc_wagi
         waga = 0
         ilosc_wagi = 0
         print(f"Ilosc utworzonych paczek: {ilosc_paczek}")
     if ilosc_wagi == 20:
         ilosc_paczek += 1
+        najwieksza_ilosc_pustych_elementow = 20 - ilosc_wagi
         waga = 0
         ilosc_wagi = 0
         print(f"Ilosc utworzonych paczek: {ilosc_paczek}")
